1fdbc472a92713d753df6dee9e84024f6358a73b
mowtwo <1046099511@qq.com>
2022-10-18 11:36:07 +0800

    修改类型讲义的定义

--- 神言.d.ts
+++ 神言.d.ts
@@ -10,6 +10,8 @@ type 那必然是 = true
 
 type 那不可能 = false
 
+type 不可窥测 = unknown
+
 export class 哈希表<键, 值> {
   钦定(圣: 键): { 为(体: 值): 管它是啥 }
   钦定(圣: 键, 体: 值): 管它是啥
@@ -25,9 +27,9 @@ export class 阴阳<值> {
   似乎(圣: 值): 也许是
 }
 
-export function 构筑<类>(ctor: new () => 类): 类
+export function 构筑<类>(原型: new () => 类): 类
 
-export function 窥探(...args: unknown[]): void
+export function 窥探(...万事万物: 不可窥测[]): void
 
 export function 引言<T>(言: T): { 言: T }
 
